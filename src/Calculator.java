58443ae50179c013050911153017792b7d48c54e
Irina <47748161t@iespoblenou.org>
2024-12-03 08:46:32 +0100

    Add modulus to feature/modulus

--- src/Calculator.java
+++ src/Calculator.java
@@ -13,6 +13,7 @@ public class Calculator {
     public int divide(int a, int b){
         return a/b;
     }
+    public int modulus(int a, int b){return a%b;}
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         System.out.println("Addition: " + calculator.add(10, 5));
